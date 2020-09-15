40ad22b277e71a84e29efb4718407f0fe5f76b94
Moruf Ajibike <abiodun.ajibike1@yahoo.com>
2020-09-15 22:04:54 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Install google chrome
-echo "-- Install Google Chrome browser ---"
-if [ -f "/Applications/Chrome.app" ]; then
-    wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
-    open ~/Downloads/googlechrome.dmg
-    sudo cp -r /Volumes/Google\ Chrome/Google\ Chrome.app /Applications/
-else
-    echo "Google chrome already installed. skipping..."
-fi
-
 # Install homebrew
 echo "--- Installing homebrew ---"
 if [ -z "$(brew --version)" ]; then
@@ -25,7 +15,7 @@ for pkg in `cat packages.txt`
         echo "--- Installing $pkg ---"
         if [ -z "$($pkg --version)" ]; then
 	    echo "Installing..."
-            #brew cask install $pkg
+            brew install $pkg
         else
             echo "$pkg already installed. skipping..."
         fi
@@ -42,9 +32,17 @@ fi
 
 # Install oh-my-zsh
 echo "--- Installing oh-my-zsh ---"
-if [ -z "$(zsh --version)" ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+/bin/bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#if [ -z "$(zsh --version)" ]; then
+#    /bin/bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#fi
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+
+echo '[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"' >> ~/.bash_profile
 
 for dotfile in .zshrc .vimrc .tmux.conf
 do
@@ -63,7 +61,37 @@ tmux source $HOME/.tmux.conf
 echo "--- Install docker ---"
 brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
 
-sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve 
-sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-docker-machine create default --driver xhyve --xhyve-experimental-nfs-share
+#sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve 
+#sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+#docker-machine create default --driver xhyve --xhyve-experimental-nfs-share
 
+echo "--- Installing $pkg ---"
+if [ -z "$($pkg --version)" ]; then
+	echo "Installing..."
+            brew install $pkg
+        else
+            echo "$pkg already installed. skipping..."
+        fi
+
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install visual-studio-code
+brew cask install docker
+
+
+# Install google chrome
+#echo "-- Install Google Chrome browser ---"
+#if [ -f /Applications/Chrome.app ]; then
+#    echo "Google chrome already installed. skipping..."
+#else
+#    echo "Downloading Google Chrome..."
+#    wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg -O ~/Downloads/googlechrome.dmg
+#    open ~/Downloads/googlechrome.dmg
+#    sudo cp -r /Volumes/Google\ Chrome/Chrome.app /Applications/Chrome.app
+#fi
+
+# (optional) set default shell
+# chsh -s /bin/zsh
+
+git config --global user.email "abiodun.ajibike1@yahoo.com"
+git config --global user.name "Moruf Ajibike"
