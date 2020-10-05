0d9d80e7c2f6fbede211258e9999c53bbaab2d58
Moruf Ajibike <abiodun.ajibike1@yahoo.com>
2020-10-05 12:50:51 +0100

    Install python3.7.4 with pyenv

--- install.sh
+++ install.sh
@@ -75,6 +75,18 @@ for pkg in `cat brew_cask_pkgs.txt`
         brew cask install $pkg
     done
 
+# Install python3.7.4 (New Macbooks now come with python3.8 as of the time of writing this)
+pyenv install 3.7.4
+pyenv global 3.7.4
+
+PYENV_PATH="$(pyenv root)/shims"
+if grep -Fxq "$PYENV_PATH" /etc/paths > /dev/null; then
+   echo "$PYENV_PATH already set in /etc/paths"
+else
+   echo $PYENV_PATH | sudo tee -a /etc/paths
+fi
+
+# create github ssh key
 ./create-ssh-key.sh
 
 git config --global user.email "abiodun.ajibike1@yahoo.com"
